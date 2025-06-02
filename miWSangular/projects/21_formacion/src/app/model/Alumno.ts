6ba6cba9362f5e34aabf0e89869007e95c9359d5
Francisco Ramírez <ramirez.martin.francisco@gmail.com>
2025-06-02 14:27:41 +0200

    20250602

---
+++ miWSangular/projects/21_formacion/src/app/model/Alumno.ts
@@ -0,0 +1,15 @@
+export class Alumno{
+  usuario:string;
+  password:string;
+  nombre:string;
+  email:string;
+  edad:number;
+
+  constructor(usuario?:string,password?:string,nombre?:string,email?:string,edad?:number){
+    this.usuario=usuario;
+    this.password=password;
+    this.nombre=nombre;
+    this.email=email;
+    this.edad=edad;
+  }
+}
---
+++ miWSangular/projects/21_formacion/src/app/service/matricula.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatriculaService {
+
+  constructor(private http:HttpClient) { }
+  urlCursos:string='http://localhost:3000/cursos/todos';
+
+}
+
+
+
+
