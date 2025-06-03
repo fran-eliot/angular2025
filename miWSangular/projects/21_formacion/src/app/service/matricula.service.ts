68fd87433825f4ff581763b4147f83d2fba0acc4
Francisco Ramírez <ramirez.martin.francisco@gmail.com>
2025-06-03 14:28:32 +0200

    20250603

--- miWSangular/projects/21_formacion/src/app/service/matricula.service.ts
+++ miWSangular/projects/21_formacion/src/app/service/matricula.service.ts
@@ -1,13 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Curso } from '../model/Curso';
+import { Alumno } from '../model/Alumno';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Matricula } from '../model/Matricula';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatriculaService {
 
+  urlBase:string = 'http://localhost:3000/formacion';
+
   constructor(private http:HttpClient) { }
-  urlCursos:string='http://localhost:3000/cursos/todos';
+
+
+  buscarCursos():Observable<Curso[]>{
+    const url:string =`${this.urlBase}/cursos`;
+
+    return this.http.get<Curso[]>(url);
+    }
+
+  buscarAlumnosNoMatriculadosenCurso(idCurso:number):Observable<Alumno[]>{
+    const url:string =`${this.urlBase}/noMatriculados/${idCurso}`;
+
+    return this.http.get<Alumno[]>(url);
+  }
+
+  matricular(matricula:Matricula):Observable<boolean>{
+     const url:string =`${this.urlBase}/matricular`;
+     return this.http.post<boolean>(url,matricula)
+     .pipe(map(()=>true),
+        catchError(e=>of(false)));
+
+
+  }
 
 }
 
